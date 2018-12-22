6efe2c12ced9a3cd802f5d19933cb86277e7e106
Markus Oberlehner <markus.oberlehner@gmail.com>
2018-12-22 09:31:09 +0100

    Render map

--- src/App.vue
+++ src/App.vue
@@ -14,8 +14,19 @@ export default {
   async mounted() {
     try {
       const google = await gmapsInit();
-      console.log(google);
+      const geocoder = new google.maps.Geocoder();
+      const map = new google.maps.Map(this.$el);
+
+      geocoder.geocode({ address: `Austria` }, (results, status) => {
+        if (status !== `OK` || !results[0]) {
+          throw new Error(status);
+        }
+
+        map.setCenter(results[0].geometry.location);
+        map.fitBounds(results[0].geometry.viewport);
+      });
     } catch (error) {
+      // eslint-disable-next-line no-console
       console.error(error);
     }
   },
