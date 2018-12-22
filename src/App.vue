c74cd5544264732c75e56a08f5226e833a84341a
Markus Oberlehner <markus.oberlehner@gmail.com>
2018-12-22 09:13:37 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="App">
+    <h1 class="App__headline">
+      Store Finder
+    </h1>
+  </div>
+</template>
+
+<script>
+import gmapsInit from './utils/gmaps';
+
+export default {
+  name: `App`,
+  async mounted() {
+    try {
+      const google = await gmapsInit();
+      console.log(google);
+    } catch (error) {
+      console.error(error);
+    }
+  },
+};
+</script>
+
+<style lang="scss">
+@import './assets/scss/settings/**/*';
+@import './assets/scss/generic/**/*';
+
+.App {
+  padding-top: setting-spacing(xl);
+  padding-bottom: setting-spacing(xl);
+}
+
+.App__headline {
+  text-align: center;
+}
+</style>
