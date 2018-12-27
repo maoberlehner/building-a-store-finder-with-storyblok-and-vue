533c2578a2cef1e2a28a473e52d99dc296f079fe
Markus Oberlehner <markus.oberlehner@gmail.com>
2018-12-27 08:08:41 +0100

    Add the StoreFinder component

--- src/App.vue
+++ src/App.vue
@@ -3,91 +3,22 @@
     <h1 class="App__headline">
       Store Finder
     </h1>
+    <StoreFinder :stores="stores"/>
   </div>
 </template>
 
 <script>
-import MarkerClusterer from '@google/markerclusterer';
-
-import gmapsInit from './utils/gmaps';
-
-const locations = [
-  {
-    position: {
-      lat: 48.160910,
-      lng: 16.383330,
-    },
-  },
-  {
-    position: {
-      lat: 48.174270,
-      lng: 16.329620,
-    },
-  },
-  {
-    position: {
-      lat: 48.146140,
-      lng: 16.297030,
-    },
-  },
-  {
-    position: {
-      lat: 48.135830,
-      lng: 16.194460,
-    },
-  },
-  {
-    position: {
-      lat: 48.306091,
-      lng: 14.286440,
-    },
-  },
-  {
-    position: {
-      lat: 47.503040,
-      lng: 9.747070,
-    },
-  },
-];
+import StoreFinder from './components/StoreFinder.vue';
 
 export default {
   name: `App`,
-  async mounted() {
-    try {
-      const google = await gmapsInit();
-      const geocoder = new google.maps.Geocoder();
-      const map = new google.maps.Map(this.$el);
-
-      geocoder.geocode({ address: `Austria` }, (results, status) => {
-        if (status !== `OK` || !results[0]) {
-          throw new Error(status);
-        }
-
-        map.setCenter(results[0].geometry.location);
-        map.fitBounds(results[0].geometry.viewport);
-      });
-
-      const markerClickHandler = (marker) => {
-        map.setZoom(13);
-        map.setCenter(marker.getPosition());
-      };
-
-      const markers = locations
-        .map((location) => {
-          const marker = new google.maps.Marker({ ...location, map });
-          marker.addListener(`click`, () => markerClickHandler(marker));
-
-          return marker;
-        });
-
-      // eslint-disable-next-line no-new
-      new MarkerClusterer(map, markers, {
-        imagePath: `https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m`,
-      });
-    } catch (error) {
-      // eslint-disable-next-line no-console
-      console.error(error);
-    }
+  components: {
+    StoreFinder,
+  },
+  data() {
+    return {
+      stores: [],
+    };
   },
 };
 </script>
---
+++ src/components/StoreFinder.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="StoreFinder">
+    <div class="StoreFinder__search">
+      <!-- StoreFinderSearch -->
+    </div>
+    <div class="StoreFinder__grid">
+      <div class="StoreFinder__list">
+        <!-- StoreFinderList -->
+      </div>
+      <div class="StoreFinder__map">
+        <!-- StoreFinderMap -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: `StoreFinder`,
+  props: {
+    stores: {
+      default: () => [],
+      required: true,
+      type: Array,
+    },
+  },
+};
+</script>
